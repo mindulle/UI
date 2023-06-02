10a0e0633392a5583be25ce15830eba6b369bea3
hoofoo1021 <hoofoo1021@gmail.com>
2023-06-02 18:55:06 +0000

    misc

---
+++ .changeset/serious-suits-exist.md
@@ -0,0 +1,8 @@
+---
+"@mindulle-ui/button": patch
+"@mindulle-ui/core": patch
+"@mindulle-ui/icons": patch
+"@mindulle-ui/tokens": patch
+---
+
+Initialize basic components packages
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 
+import tsconfig from './tsconfig.json';
 import { cssImport } from './vite-plugin-css'
+const paths = tsconfig.compilerOptions.paths;
+
+import packageJSON from './package.json';
 
 export default defineConfig({
-  plugins: [cssImport()]
-  
+  plugins: [cssImport()],
+  build: {
+    lib: {
+      entry: packageJSON.source,
+      formats: ['es', 'cjs'],
+      fileName: (format) => (format === 'es' ? 'index.es.js' : 'index.js'),
+    },
+  }
 })
+
